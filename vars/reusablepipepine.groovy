7a7f841a9c925b13e5643f36ba7d104204f1aba5
veeraz99 <sk4359745@gmail.com>
2025-12-01 19:05:40 +0000

    added new file to lib

---
+++ vars/reusablepipepine.groovy
@@ -0,0 +1,42 @@
+def call {string giturl,string github token}
+pipeline {
+    agent { label "slave-1" }
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: '*/main']],
+                    userRemoteConfigs: [[
+                        url: giturl,
+                        credentialsId: 'github token'
+                    ]]
+                ])
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh 'mvn clean install'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+
+        stage('Deploy to Artifactory') {
+            steps {
+                configFileProvider(
+                    [configFile(fileId: "322cfcc8-59c4-4cc5-90b6-cef808c15e9b", variable: 'MAVEN_SETTINGS')]
+                ) {
+                    sh 'mvn deploy -s $MAVEN_SETTINGS'
+                }
+            }
+        }
+    }
+}
